4a8a7a4ba4dac214dd12252f225231c611003b71
Abigiya Tadesse <Abigiyatadesse23@gmail.com>
2022-11-03 14:28:55 +0300

    Change value

--- queries.sql
+++ queries.sql
@@ -3,7 +3,7 @@ SELECT name from animals WHERE name like '%mon';
 SELECT name from animals WHERE date_of_birth >= '2016-01-01' and date_of_birth <= '2019-12-31';
 SELECT name from animals WHERE neutered = true and escape_attempts < 3;
 SELECT date_of_birth from animals WHERE name IN ('Agumon', 'Pikachu');
-SELECT name, escape_attempts from animals WHERE weight_kg > 10.4; 
+SELECT name, escape_attempts from animals WHERE weight_kg > 10.5; 
 SELECT name from animals WHERE neutered;
 SELECT name from animals WHERE name NOT ILIKE '%Gabumon%';
 SELECT name from animals WHERE weight_kg >= 10.4 and weight_kg <= 17.3;
